c672f8c82bed7e4fa4ef9434d953994f1e638b62
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-07-05 01:51:33 +0100

    Update File

---
+++ ExtrasOOP/LinkingObjects/linked_boxes.py
@@ -0,0 +1,12 @@
+# Linked Boxes
+
+words = ["Bicycle", "Elections", "Popularity", "Football", "Airplane"]
+
+
+class Box:
+
+    def __init__(self, word, letter_index, letter):
+        self.word = word
+        self.letter_index = letter_index
+        self.letter = letter
+
