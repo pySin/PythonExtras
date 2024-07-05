183d6ba743dacca18e65528af1b95f012cd02628
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-07-05 15:59:20 +0100

    Update File

--- ExtrasOOP/LinkingObjects/linked_boxes.py
+++ ExtrasOOP/LinkingObjects/linked_boxes.py
@@ -6,11 +6,11 @@ words = ["Bicycle", "Elections", "Popularity", "Football", "Airplane"]
 
 class Box:
 
-    def __init__(self, word, letter_index, letter):
+    def __init__(self, word, letter_index, letter, previous_box):
         self.word = word
         self.letter_index = letter_index
         self.letter = letter
-        self.previous_box: Optional[Box, bool] = None
+        self.previous_box: Optional[Box, bool] = previous_box
 
 
 class BoxContainer:
@@ -24,7 +24,8 @@ class Reading:
     def __init__(self, c_words: list[str]):
         self.c_words = c_words
         self.box_container = BoxContainer()
+        self.previous_box = None
 
     def add_step(self, word, letter_index, letter):
-        current_box = Box(word, letter_index, letter)
+        current_box = Box(word, letter_index, letter, self.previous_box)
         self.box_container.container.append(current_box)
