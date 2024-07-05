54a5db75a7a3572a4756eafc0df630e5260a61d6
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-07-05 01:59:13 +0100

    Update File

--- ExtrasOOP/LinkingObjects/linked_boxes.py
+++ ExtrasOOP/LinkingObjects/linked_boxes.py
@@ -1,4 +1,5 @@
 # Linked Boxes
+from typing import Optional
 
 words = ["Bicycle", "Elections", "Popularity", "Football", "Airplane"]
 
@@ -9,4 +10,14 @@ class Box:
         self.word = word
         self.letter_index = letter_index
         self.letter = letter
+        self.previous_box: Optional[Box, bool] = None
+
+
+class BoxContainer:
+
+    def __init__(self, box: Box):
+        self.box: Box = box
+        self.container = []
+
+
 
