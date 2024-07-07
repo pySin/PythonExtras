abd52ce63522abb04346133df4726bb45cc37a96
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-07-07 01:28:02 +0100

    Update File

--- ExtrasOOP/packaging_objects/word_articulation_style.py
+++ ExtrasOOP/packaging_objects/word_articulation_style.py
@@ -3,3 +3,40 @@
 from typing import Optional
 
 words = ["Bicycle", "Elections", "Popularity", "Football", "Airplane"]
+
+
+class Letter:
+
+    letter_articulations = {
+        "a": "open vowel",
+        "b": "bilabial consonant",
+        "c": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+        "v": "teeth labial",
+    }
+
+    def __init__(self, word, letter):
+        self.word = word
+        self.letter = letter
+        self.articulation = None
+
