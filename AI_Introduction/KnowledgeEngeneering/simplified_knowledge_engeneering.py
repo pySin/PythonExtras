318ec8c42ae45386defb31aa3dbe614668f520a4
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-07-24 12:27:03 +0100

    Update File

--- AI_Introduction/KnowledgeEngeneering/simplified_knowledge_engeneering.py
+++ AI_Introduction/KnowledgeEngeneering/simplified_knowledge_engeneering.py
@@ -6,6 +6,13 @@ class PropositionalElement:
 
 class GenerateModels:
     def __init__(self, *elements):
-        self.elements = list(elements)
+        self.elements: list = list(elements)
+        self.model: list = []
 
+    def generate(self):
 
+        for element in self.elements:
+            current_model = {}
+            for element_2_iteration in self.elements:
+                for tf in [True, False]:
+                    current_model[element_2_iteration] = tf
