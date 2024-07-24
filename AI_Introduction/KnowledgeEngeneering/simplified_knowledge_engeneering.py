3101817c3b56e9b47a23a3cee34919e3e9fee601
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-07-24 19:13:10 +0100

    Update File

--- AI_Introduction/KnowledgeEngeneering/simplified_knowledge_engeneering.py
+++ AI_Introduction/KnowledgeEngeneering/simplified_knowledge_engeneering.py
@@ -1,3 +1,5 @@
+sentence = "If it rains Ivan will visit Kolio, otherwise Nikolai"
+
 permutations = [{"Ivan": True, "Kolio": True, "Nikolai": True, "rain": True},
                 {"Ivan": True, "Kolio": False, "Nikolai": True, "rain": True},
                 {"Ivan": True, "Kolio": True, "Nikolai": False, "rain": True},
@@ -18,9 +20,9 @@ permutations = [{"Ivan": True, "Kolio": True, "Nikolai": True, "rain": True},
                 {"Ivan": True, "Kolio": False, "Nikolai": False, "rain": False},
                 ]
 
+# "If it rains Ivan will visit Kolio, otherwise Nikolai"
 for model in permutations:
-    if model["Ivan"] == True and model["Kolio"] == True and model["rain"] == True \
-            and model["Nikolai"] == False:
+    if model["Nikolai"] == True and not(model["Nikolai"] == True and model["rain"] == True):
         print(f"Model found: {model}")
 
 
