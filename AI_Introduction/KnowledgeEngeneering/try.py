55047502df824626cb254a377a44f3a39072311d
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-07-27 11:28:35 +0100

    Update File

--- AI_Introduction/KnowledgeEngeneering/try.py
+++ AI_Introduction/KnowledgeEngeneering/try.py
@@ -8,36 +8,50 @@
 #         pairs.append([names[j], False])
 #     print(pairs)
 
-names_true = ["Ivan", True], ["Kolio", True], ["Nikolai", True], ["rain", True],
-names_false = ["Ivan", False], ["Kolio", False], ["Nikolai", False], ["rain", False]
+# names_true = ["Ivan", True], ["Kolio", True], ["Nikolai", True], ["rain", True],
+# names_false = ["Ivan", False], ["Kolio", False], ["Nikolai", False], ["rain", False]
+#
+# combinations = []
+#
+# for i in range(len(names_true)):
+#     row = []
+#     for j in range(len(names_false)):
+#         inner_row = []
+#         for q in range(len(names_true)):
+#             if i == j:
+#                 inner_row.append(names_true[q])
+#             elif j == q:
+#                 inner_row.append(names_false[q])
+#             elif i == q:
+#                 inner_row.append(names_false[q])
+#             elif i != j:
+#                 inner_row.append(names_true[q])
+#         row.append(inner_row)
+#     combinations.append(row)
+#
+#
+# all_combinations = []
+# for combs in combinations:
+#     for comb in combs:
+#         all_combinations.append(comb)
+#
+#
+# print(len(combinations))
+# print(len(all_combinations))
+# print(all_combinations)
 
-combinations = []
+# true false combinations
 
-for i in range(len(names_true)):
-    row = []
-    for j in range(len(names_false)):
-        inner_row = []
-        for q in range(len(names_true)):
-            if i == j:
-                inner_row.append(names_true[q])
-            elif j == q:
-                inner_row.append(names_false[q])
-            elif i == q:
-                inner_row.append(names_false[q])
-            elif i != j:
-                inner_row.append(names_true[q])
-        row.append(inner_row)
-    combinations.append(row)
+models = []
+for i in [True, True, True, True]:
+    current_model = []
+    for j in [False, False, False, False]:
+        if i == j:
+            current_model.append(i)
+        else:
+            current_model.append(j)
+    models.append(current_model)
 
-
-all_combinations = []
-for combs in combinations:
-    for comb in combs:
-        all_combinations.append(comb)
-
-
-print(len(combinations))
-print(len(all_combinations))
-print(all_combinations)
-
+for m in models:
+    print(m)
 
