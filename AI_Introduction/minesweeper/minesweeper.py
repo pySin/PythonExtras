9e63969fa4d2c67fbb4084ff72da62da2e907f17
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-08-10 17:41:24 +0100

    Update File

--- AI_Introduction/minesweeper/minesweeper.py
+++ AI_Introduction/minesweeper/minesweeper.py
@@ -1,2 +1,33 @@
 import itertools
 import random
+
+class Minesweeper():
+    """
+    Minesweeper game representation
+    """
+
+    def __init__(self, height=8, width=8, mines=8):
+
+        # Set initial width, height, and number of mines
+        self.height = height
+        self.width = width
+        self.mines = set()
+
+        # Initialize an empty field with no mines
+        self.board = []
+        for i in range(self.height):
+            row = []
+            for j in range(self.width):
+                row.append(False)
+            self.board.append(row)
+
+        # Add mines randomly
+        while len(self.mines) != mines:
+            i = random.randrange(height)
+            j = random.randrange(width)
+            if not self.board[i][j]:
+                self.mines.add((i, j))
+                self.board[i][j] = True
+
+        # At first, player has found no mines
+        self.mines_found = set()
