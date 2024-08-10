b05aa3afdcc3b0d4d93b9a680aedf987d364c5b0
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-08-10 18:09:04 +0100

    Update File

--- AI_Introduction/minesweeper/minesweeper.py
+++ AI_Introduction/minesweeper/minesweeper.py
@@ -59,7 +59,6 @@ class Minesweeper():
         not including the cell itself.
         """
 
-
         # Keep count of nearby mines
         count = 0
 
@@ -81,3 +80,36 @@ class Minesweeper():
     def won(self):
         # Check if all mines are flagged
         return self.mines_found == self.mines
+
+
+class Sentence():
+    """
+    Logical statement about a Minesweeper game
+    A sentence consists of a set of board cells,
+    and a count of the number of those cells which are mines.
+    """
+
+    def __init__(self, cells, count):
+        self.cells = set(cells)
+        self.count = count
+
+    def __eq__(self, other):
+        return self.cells == other.cells and self.count == other.count
+
+    def __str__(self):
+        return f"{self.cells} = {self.count}"
+
+    def known_mines(self):
+        # Returns the set of all cells in self.cells known to be mines.
+        raise NotImplementedError
+
+    def known_safes(self):
+        # Returns the set of all cells in self.cells known to be safe.
+        raise NotImplementedError
+
+    def mark_mine(self, cell):
+        """
+        Updates internal knowledge representation given the fact that
+        a cell is known to be a mine.
+        """
+        raise NotImplementedError
