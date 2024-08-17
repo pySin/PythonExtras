dbde3ca3150ae2febb56a0692fedf99ea76102ca
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-08-17 19:34:28 +0100

    Update File

--- AI_Introduction/minesweeper/minesweeper_test.py
+++ AI_Introduction/minesweeper/minesweeper_test.py
@@ -11,6 +11,15 @@ expectedWinPercent = 85
 
 # Win rate necessary 90 percent / `pytest -s`
 
+
 @pt.mark.parametrize("execution_number", range(10))
 def test(execution_number):
     return play1000()
+
+
+# Complimentary function
+
+def play1000():
+    totalWon = 0
+    for _ in range(1000):
+        totalWon += play()
