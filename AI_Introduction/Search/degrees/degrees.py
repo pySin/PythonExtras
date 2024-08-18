98eb57e5dc387786944e8d635a225e6e365fc57b
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-08-18 22:25:53 +0100

    Update File

--- AI_Introduction/Search/degrees/degrees.py
+++ AI_Introduction/Search/degrees/degrees.py
@@ -1,1 +1,14 @@
 # Degrees Harvard SC50 AI
+import csv
+import sys
+
+from util import Node, StackFrontier, QueueFrontier
+
+# Maps names to a set of corresponding person_ids
+names = {}
+
+# Maps person_ids to a dictionary of: name, birth, movies (a set of movie_ids)
+people = {}
+
+# Maps movie_ids to a dictionary of: title, year, stars (a set of person_ids)
+movies = {}
