a2374211666e2dc28386164b894efdfe0977f73d
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-08-27 16:38:08 +0300

    Update File

--- AI_Introduction/Search/degrees/checks.py
+++ AI_Introduction/Search/degrees/checks.py
@@ -18,3 +18,18 @@ class EvaluationCheck:
     def empty(self):
         return len(self.nums) == 0
 
+    def remove_num(self):
+        self.nums.pop()
+
+
+numbers = [1, 2, 3, 4, 5]
+ec = EvaluationCheck(numbers)
+for i in range(len(numbers) + 1):
+    print(f"Empty expression: {ec.empty()}")
+    if ec.empty():
+        print(f"Numbers Finished!")
+        break
+    print(f"Numbers: {ec.nums}")
+    ec.remove_num()
+
+
