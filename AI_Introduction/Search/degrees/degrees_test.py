1205432741fa7a9a9a7d488c1ab3ff6109a915cd
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-08-27 15:14:47 +0300

    Update File

--- AI_Introduction/Search/degrees/degrees_test.py
+++ AI_Introduction/Search/degrees/degrees_test.py
@@ -1,3 +1,11 @@
 # Test for "degrees" challenge of Harvard's CS50AI course
 
 from degrees import load_data, person_id_for_name, shortest_path
+
+load_data("large")
+
+
+def test0():
+    source = person_id_for_name("Jennifer Lawrence")
+    target = person_id_for_name("Tom Hanks")
+    assert len(shortest_path(source, target)) == 2
