fac8f6b35693f564e530a5bb477f46f85738da4a
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-08-27 16:04:25 +0300

    Update File

--- AI_Introduction/Search/degrees/degrees_test.py
+++ AI_Introduction/Search/degrees/degrees_test.py
@@ -8,3 +8,17 @@ def test0():
     source = person_id_for_name("Jennifer Lawrence")
     target = person_id_for_name("Tom Hanks")
     assert len(shortest_path(source, target)) == 2
+
+
+def test1():
+    source = person_id_for_name("Emma Watson")
+    target = person_id_for_name("Jennifer Lawrence")
+    assert len(shortest_path(source, target)) == 3
+
+
+def test_zero_degree():
+    source = person_id_for_name("Tim Zinnemann")
+    target = person_id_for_name("Lahcen Zinoun")
+    assert shortest_path(source, target) is None
+
+
